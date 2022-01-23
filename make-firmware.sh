ff7268d00957afb0cb7e0b32edaaf3d5bc742177
Peter Altosaar <peter.altosaar@gmail.com>
2022-01-22 23:00:06 -0500

    "Remove keymap.c from QMK folder when compiled"

--- make-firmware.sh
+++ make-firmware.sh
@@ -3,5 +3,5 @@
 cp keymap.c ../qmk_firmware/keyboards/ergodox_ez/keymaps/peteraltosaar/keymap.c
 cd ../qmk_firmware
 make ergodox_ez:peteraltosaar
-#rm keyboards/ergodox_ez/keymaps/peteraltosaar/keymap.c
+rm keyboards/ergodox_ez/keymaps/peteraltosaar/keymap.c
 cd -
